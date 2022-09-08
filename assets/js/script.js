af8634d41fbfdf8b57a3f450e4a7b446ad40ddbf
unknown <tfb1223@gmail.com>
2022-09-08 13:57:25 -0500

    added array of questions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,77 @@
 var startQuiz = function () {
-    document.getElementById("questions").innerHTML=
-        "<h1>Which of the following methods is used to access HTML elements using Javascript?</h1> <button>1. getElementId()</button> <button>2. getElementById()</button> <button>3. getElementsByClassName()</button> <button id=answer>4. Both 2 and 3</button>";
-        if (document.getElementById("#answer").clicked === true) {
-            console.log("Found You!")
-        }
-    //document.getElementById("questions").innerHTML=
-     //   "<h1>When the switch statement matches the expression with the given labels, how is the comparison done?</h1> <button id=answers>1. Both the datatype and the result of the expression are compared.</button> <button id=answers>2. Only the datatype of the expression is compared.</button> <button id=answers>3. Only the value of the expression is compared.</button> <button id=answers>4. None of the above.</button>";
-}
+    document.getElementById("questions").innerHTML = '<h1>' + quizInfo.question + '</h1>' + "<button>" + quizInfo.answers + "</button>";
+};
+var quizInfo = [
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answers: "1. getElementId()",
+        answers: "2. getElementById()",
+        answers: "3. getElementsByClassName()",
+        answers: "4. Both 2 and 3"
+    },
+    {
+        question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
+        answers: "1. Both the datatype and the result of the expression are compared.",
+        answers: "2. Only the datatype of the expression is compared.",
+        answers: "3. Only the value of the expression is compared.",
+        answers: "4. None of the above."
+    },
+    {
+        question: "What does the ‘toLocateString()’ method do in JS?",
+        answers: "Returns a localised object representation.",
+        answers: "Returns a parsed string.",
+        answers: "Returns a localized string representation of an object.",
+        answersw: "None of the above."
+    },
+    {
+        question: "The 3 basic object attributes in Javascript are:",
+        answersw: "Class, prototype, objects' parameters.",
+        answers: "Class, prototype, objects' extensible flag.",
+        answersw: "Class, parameters, objects' extensible flag.",
+        answers: "Classes, Native object, and Interfaces and Object's extensible flag."
+    },
+    {
+        question: "Which of the following are closures in Javascript?",
+        answers: "Variables",
+        answers: "Functions",
+        answers: "Objects",
+        answers: "All of the above"
+    },
+    {
+        question: "What keyword is used to declare an asynchronous function in Javascript?",
+        answers: "async",
+        answersw: "await",
+        answersw: "setTimeout",
+        answersw: "None of the above"
+    },
+    {
+        question: "How to stop an interval timer in Javascript?",
+        answersw: "intervalOver",
+        answersw: "clearTimer",
+        answers: "clearInterval",
+        answersw: "Cleartime"
+    },
+    {
+        question: "How do we write a comment in javascript?",
+        answersw: "/* */",
+        answers: "//",
+        answersw: "#",
+        answersw: "&&"
+    },
+    {
+        question: "Which of the following are not server-side Javascript objects?",
+        answersw: "Date",
+        answersw: "FileUpload",
+        answersw: "Function",
+        answers: "All of the above"
+    },
+    {
+        question: "Which object in Javascript doesn’t have a prototype?",
+        answers: "Base Object",
+        answersw: "All objects have a prototype",
+        answersw: "None of the objects have a prototype",
+        answersw: "None of the above"
+    }
+]
 var startBtn = document.querySelector("#start")
 startBtn.addEventListener("click", startQuiz)
