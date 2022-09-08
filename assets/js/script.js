7123025f22ea3447b7fad352736d4e4b19943775
unknown <tfb1223@gmail.com>
2022-09-08 15:22:53 -0500

    working on getting array to work properly need a break

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,83 @@
 var startQuiz = function () {
-    document.getElementById("questions").innerHTML = '<h1>' + quizInfo.question + '</h1>' + "<button>" + quizInfo.answers + "</button>";
+    for (var i = 0; i < quizInfo.length; i++) {
+        if (time > 75){
+        var quizI = quizInfo[i]
+        document.getElementById("questions").innerHTML = 
+        '<h1>' + quizI.question + '</h1>' + "<button>" + quizI.answers1 + "</button>" + '<button>' + quizI.answers2 + '</button>' + "<button>" + quizI.answers3 + "</button>" + '<button>' + quizI.answers4 + '</button>';
+        }
+    }
+    var time = document.getElementById("timer").innerHTML="75";
 };
 var quizInfo = [
     {
         question: "Which of the following methods is used to access HTML elements using Javascript?",
-        answers: "1. getElementId()",
-        answers: "2. getElementById()",
-        answers: "3. getElementsByClassName()",
-        answers: "4. Both 2 and 3"
+        answers1: "1. getElementId()",
+        answers2: "2. getElementById()",
+        answers3: "3. getElementsByClassName()",
+        answers4: "4. Both 2 and 3"
     },
     {
         question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
-        answers: "1. Both the datatype and the result of the expression are compared.",
-        answers: "2. Only the datatype of the expression is compared.",
-        answers: "3. Only the value of the expression is compared.",
-        answers: "4. None of the above."
+        answers1: "1. Both the datatype and the result of the expression are compared.",
+        answers2: "2. Only the datatype of the expression is compared.",
+        answers3: "3. Only the value of the expression is compared.",
+        answers4: "4. None of the above."
     },
     {
-        question: "What does the ‘toLocateString()’ method do in JS?",
-        answers: "Returns a localised object representation.",
-        answers: "Returns a parsed string.",
-        answers: "Returns a localized string representation of an object.",
-        answersw: "None of the above."
+        question: "What does the 'toLocateString()' method do in JS?",
+        answers1: "Returns a localised object representation.",
+        answers2: "Returns a parsed string.",
+        answers3: "Returns a localized string representation of an object.",
+        answers4: "None of the above."
     },
     {
         question: "The 3 basic object attributes in Javascript are:",
-        answersw: "Class, prototype, objects' parameters.",
-        answers: "Class, prototype, objects' extensible flag.",
-        answersw: "Class, parameters, objects' extensible flag.",
-        answers: "Classes, Native object, and Interfaces and Object's extensible flag."
+        answers1: "Class, prototype, objects' parameters.",
+        answers2: "Class, prototype, objects' extensible flag.",
+        answers3: "Class, parameters, objects' extensible flag.",
+        answers4: "Classes, Native object, and Interfaces and Object's extensible flag."
     },
     {
         question: "Which of the following are closures in Javascript?",
-        answers: "Variables",
-        answers: "Functions",
-        answers: "Objects",
-        answers: "All of the above"
+        answers1: "Variables",
+        answers2: "Functions",
+        answers3: "Objects",
+        answers4: "All of the above"
     },
     {
         question: "What keyword is used to declare an asynchronous function in Javascript?",
-        answers: "async",
-        answersw: "await",
-        answersw: "setTimeout",
-        answersw: "None of the above"
+        answers1: "async",
+        answers2: "await",
+        answers3: "setTimeout",
+        answers4: "None of the above"
     },
     {
         question: "How to stop an interval timer in Javascript?",
-        answersw: "intervalOver",
-        answersw: "clearTimer",
-        answers: "clearInterval",
-        answersw: "Cleartime"
+        answers1: "intervalOver",
+        answers2: "clearTimer",
+        answers3: "clearInterval",
+        answers4: "Cleartime"
     },
     {
         question: "How do we write a comment in javascript?",
-        answersw: "/* */",
-        answers: "//",
-        answersw: "#",
-        answersw: "&&"
+        answers1: "/* */",
+        answers2: "//",
+        answers3: "#",
+        answers4: "&&"
     },
     {
         question: "Which of the following are not server-side Javascript objects?",
-        answersw: "Date",
-        answersw: "FileUpload",
-        answersw: "Function",
-        answers: "All of the above"
+        answers1: "Date",
+        answers2: "FileUpload",
+        answers3: "Function",
+        answers4: "All of the above"
     },
     {
-        question: "Which object in Javascript doesn’t have a prototype?",
-        answers: "Base Object",
-        answersw: "All objects have a prototype",
-        answersw: "None of the objects have a prototype",
-        answersw: "None of the above"
+        question: "Which object in Javascript doesn't have a prototype?",
+        answers1: "Base Object",
+        answers2: "All objects have a prototype",
+        answers3: "None of the objects have a prototype",
+        answers4: "None of the above"
     }
 ]
 var startBtn = document.querySelector("#start")
