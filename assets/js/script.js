647413c2bed3ecb182a0a9b206b06dd41a706996
unknown <tfb1223@gmail.com>
2022-09-09 07:15:00 -0500

    the answers arent working properly think I know the solution

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,25 @@
 
+
+var startQuiz = function () {
 // setting variables
 var i = 0;
 var j = 0;
-var correctAnswer = 0;
-var startQuiz = function () {
-
-    var quizI = quizInfo[i]
+//var correctAnswer = 0;
+    var quizI = quizInfo[i];
     document.getElementById("questions").innerHTML =
-        '<h1>' + quizI.question + '</h1>' + "<button id=choices>" + quizI.answers[j] + "</button>" + "<button id=choices>" + quizI.answers[j + 1] + "</button>" + "<button id=choices>" + quizI.answers[j + 2] + "</button>" + "<button id=choices>" + quizI.answers[j + 3] + "</button>";
-    var nextQue = document.getElementById("choices")
+        '<h1>' + quizI.question + '</h1>' + "<button id=choices>" + quizI.answers[j] + "</button>" + '<button id=choices>' + quizI.answers[j + 1] + '</button>' + "<button id=choices>" + quizI.answers[j + 2] + "</button>" + '<button id=choices>' + quizI.answers[j + 3] + '</button>';
 
-    nextQue.onclick = function () {
+    // function to go through array
+    var nextQue = function () {
         if (i > quizInfo.length - 1) {
             i = 0;
-        }
+        };
         startQuiz(i);
-        i++
+        i++ 
     };
+    var next = document.getElementById("choices");
+    next.addEventListener("click", nextQue);
+    console.log(next);
 };
 
 // array for the quiz to use
@@ -27,7 +30,7 @@ var quizInfo = [
             "getElementById()",
             "getElementsByClassName()",
             "Both 2 and 3"],
-        correctAnswer: 4
+        correctAnswer: 3
     },
     {
         question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
@@ -35,7 +38,7 @@ var quizInfo = [
             "Only the datatype of the expression is compared.",
             "Only the value of the expression is compared.",
             "None of the above."],
-        correctAnswer: 1
+        correctAnswer: 0
     },
     {
         question: "What does the 'toLocateString()' method do in JS?",
@@ -43,7 +46,7 @@ var quizInfo = [
             "Returns a parsed string.",
             "Returns a localized string representation of an object.",
             "None of the above."],
-        correctAnswer: 3
+        correctAnswer: 2
     },
     {
         question: "The 3 basic object attributes in Javascript are:",
@@ -51,7 +54,7 @@ var quizInfo = [
             "Class, prototype, objects' extensible flag.",
             "Class, parameters, objects' extensible flag.",
             "Classes, Native object, and Interfaces and Object's extensible flag."],
-        correctAnswer: [2, 4]
+        correctAnswer: [1, 3]
     },
     {
         question: "Which of the following are closures in Javascript?",
@@ -59,7 +62,7 @@ var quizInfo = [
             "Functions",
             "Objects",
             "All of the above"],
-        correctAnswer: 4
+        correctAnswer: 3
     },
     {
         question: "What keyword is used to declare an asynchronous function in Javascript?",
@@ -67,7 +70,7 @@ var quizInfo = [
             "await",
             "setTimeout",
             "None of the above"],
-        correctAnswer: 1
+        correctAnswer: 0
     },
     {
         question: "How to stop an interval timer in Javascript?",
@@ -75,7 +78,7 @@ var quizInfo = [
             "clearTimer",
             "clearInterval",
             "Cleartime"],
-        correctAnswer: 3
+        correctAnswer: 2
     },
     {
         question: "How do we write a comment in javascript?",
@@ -83,7 +86,7 @@ var quizInfo = [
             "//",
             "#",
             "&&"],
-        correctAnswer: 2
+        correctAnswer: 1
     },
     {
         question: "Which of the following are not server-side Javascript objects?",
@@ -91,7 +94,7 @@ var quizInfo = [
             "FileUpload",
             "Function",
             "All of the above"],
-        correctAnswer: 4
+        correctAnswer: 3
     },
     {
         question: "Which object in Javascript doesn't have a prototype?",
@@ -99,7 +102,7 @@ var quizInfo = [
             "All objects have a prototype",
             "None of the objects have a prototype",
             "None of the above"],
-        correctAnswer: 1
+        correctAnswer: 0
     }
 ];
 var startBtn = document.querySelector("#start")
